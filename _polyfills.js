84081ba4e0220e16fe48271337c7161a44ab63c1
Kareem Kibue <kareemkibue@gmail.com>
2018-02-09 11:06:04 -0500

    Add webkit polyfill

---
+++ _polyfills.js
@@ -0,0 +1,15 @@
+$(window).load(function() {
+
+    //*************** WEBKIT POLYFILL - SKIP TO NAV ***************//
+    window.addEventListener('hashchange', function(event) {
+        var element = document.getElementById(location.hash.substring(1));
+        if (element) {
+            if (!/^(?:a|select|input|button|textarea)$/i.test(element.tagName)) {
+                element.tabIndex = -1;
+            }
+            element.focus();
+        }
+
+    }, false);
+
+});
